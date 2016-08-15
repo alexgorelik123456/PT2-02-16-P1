34879fd433b42213ef2ed01fc738a800e983ded5
Alexander Gorelik <alexander.gorelik84@gmail.com>
2016-08-15 12:50:37 +0300

    Update PT2-02-16-P5.py

--- PT2-02-16-P5.py
+++ PT2-02-16-P5.py
@@ -1,4 +1,21 @@
-def sum():
-    print('Hello sum')
 
-sum()
+import inflect
+def number(x):
+    p = inflect.engine()
+    print(p.number_to_words(x))
+number(987)
+
+
+from num2words import num2words
+def number2(y):
+    print(num2words(y, lang='de'))
+number2(789)
+
+
+
+def spam(numb):
+    print('egg' * numb)
+
+spam(1)
+spam(3)
+spam(12)
